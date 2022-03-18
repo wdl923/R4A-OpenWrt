733d5b97f248d9558f012b37b09c03f3fae546ad
wdl923 <wdl923@users.noreply.github.com>
2022-03-18 10:30:56 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,4 +11,16 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.31.1/g' package/base-files/files/bin/config_generate
+
+# Modify Host Name
+sed -i '/uci commit system/i\uci set system.@system[0].hostname='MiR4A'' package/lean/default-settings/files/zzz-default-settings
+
+# Modify Theme
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+
+# Modify Empty Password
+sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz-default-setting
+
+# Modify WiFi SSID
+sed -i 's/ssid=OpenWrt/ssid=OpenWrt/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
